22b2024cff2a7008ccbf82c194fd75024b2439f5
surya53038 <surya53038@gmail.com>
2026-01-11 22:57:30 +0530

    working on the backend

--- backend/ai_blog_app/ai_blog_generator/views.py
+++ backend/ai_blog_app/ai_blog_generator/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    pass
+    return render(request, 'index.html')
+
+def user_login(request):
+    return render (request,'login.html')
+
+def user_signup(request):
+    return render (request, 'signup.html')
+
+def user_logout(request):
+    return render (request, 'logout.html')
